55e8cfb261b97237f4b4906fdfe4e9557bc16cf5
Navin Jomi K <23bca028@caias.in>
2026-05-27 01:18:18 +0530

    Added AddTwoNumbers Program

---
+++ AddTwoNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number1 = 16;
+        int number2 = 45;
+        int sum = number1 + number2;
+        Console.WriteLine("Sum of number1 and number 2 is:" + sum);
+    }
+}
