d78c9215d6057d3da260e6ac0728a7a4db3f7930
Navin Jomi K <23bca028@caias.in>
2026-05-27 01:24:03 +0530

    put number1 and number2 to be user input values

--- AddTwoNumbers/Program.cs
+++ AddTwoNumbers/Program.cs
@@ -4,8 +4,10 @@ class Program
 {
     static void Main()
     {
-        int number1 = 16;
-        int number2 = 45;
+        Console.WriteLine("Enter number1:");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter number2:");
+        int number2 = Convert.ToInt32(Console.ReadLine());
         int sum = number1 + number2;
         Console.WriteLine("Sum of number1 and number 2 is:" + sum);
     }
