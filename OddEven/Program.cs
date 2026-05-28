e0e9d54289396b1ae35fd1bed278dfff33fc3d8d
Navin Jomi K <23bca028@caias.in>
2026-05-29 00:38:26 +0530

    Added OddEven Program

---
+++ OddEven/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number:");
+        int n=Convert.ToInt32(Console.ReadLine());
+
+        if (n % 2 == 0)
+        {
+            Console.WriteLine("Even number");
+        }
+        else
+        {
+            Console.WriteLine("Odd number");
+        }
+    }
+}
