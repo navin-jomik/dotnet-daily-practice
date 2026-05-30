1e581f1ae759c74b4416df70d5c04a621a1b7054
Navin Jomi K <23bca028@caias.in>
2026-05-31 00:14:09 +0530

    Added AddCalculator Program

---
+++ AddCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Simple Calculator of addition");
+
+        Console.WriteLine("Enter first number:");
+        double number1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter second number:");
+        double number2 = Convert.ToDouble(Console.ReadLine());
+
+        double result = number1 + number2;
+
+        Console.WriteLine("The sum of two double values entered is:" + result);
+    }
+}
