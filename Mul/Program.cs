56a1d521bcefc134b4b9443c4e9e8290000e5e07
Navin Jomi K <23bca028@caias.in>
2026-05-31 00:41:11 +0530

    Added Mul program

---
+++ Mul/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number:");
+        int target = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("The multiplication table for:" + target);
+        for(int i = 1; i <= 12; i++)
+        {
+            int product = target * i;
+            Console.WriteLine("The product of " + target + " is " + product);
+        }
+    }
+}
