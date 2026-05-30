871ba24d4466250b20705a551528bbd0727fe394
Navin Jomi K <23bca028@caias.in>
2026-05-31 00:21:05 +0530

    Added EvenOdd with Int.Parse as converter

---
+++ EvenOdd/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if(number % 2 == 0)
+        {
+            Console.WriteLine("The entered number is a even number");
+        }
+        else
+        {
+            Console.WriteLine("The entered number is a odd number");
+        }
+    }
+}
